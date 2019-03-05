c2a066ae9d08835256ab8eedf2e6235d997cceda
Rico Lang <rico.lang@mackevision.com>
2019-03-05 17:53:42 +0100

    Add function to find by uuid and user

--- src/main/java/de/rlang/documentservice/repository/ProjectRepository.java
+++ src/main/java/de/rlang/documentservice/repository/ProjectRepository.java
@@ -1,9 +1,16 @@
 package de.rlang.documentservice.repository;
 
 import de.rlang.documentservice.model.entity.Project;
+import de.rlang.documentservice.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Project findByUuid(UUID uuid);
+
+    List<Project> findAllByUser(User user);
 }
