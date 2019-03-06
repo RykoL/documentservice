63f6dbba323ae23c369c08b2fbd27145bfea4ba5
Rico Lang <rico.lang@mackevision.com>
2019-03-06 17:59:34 +0100

    Add function to delete a project

--- src/main/java/de/rlang/documentservice/service/ProjectService.java
+++ src/main/java/de/rlang/documentservice/service/ProjectService.java
@@ -6,34 +6,24 @@ import de.rlang.documentservice.model.entity.Project;
 import de.rlang.documentservice.model.entity.User;
 import de.rlang.documentservice.repository.ProjectRepository;
 import de.rlang.documentservice.util.EntityToDTOConverter;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.web.annotation.ExposableControllerEndpoint;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.parser.Entity;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
 
 
-    private final ProjectRepository projectRepository;
-
-    private final UserService userService;
+    @Autowired
+    private ProjectRepository projectRepository;
 
     @Autowired
-    public ProjectService(
-            ProjectRepository projectRepository,
-            UserService userService) {
-       this.projectRepository = projectRepository;
-       this.userService = userService;
-    }
+    private UserService userService;
 
     @Transactional
     public ProjectInformationDTO createProject(CreateProjectDTO createProjectDTO) {
@@ -59,9 +49,17 @@ public class ProjectService {
     public List<ProjectInformationDTO> getAllProjects() {
         User currentUser = userService.getCurrentAuthenticatedUser();
 
-        List<Project> projects = projectRepository.findAllByUser(currentUser);
+        List<Project> projects = projectRepository.findAllByCreator(currentUser);
 
         return projects.stream()
                 .map(EntityToDTOConverter::ConvertToProjectInformationDTO).collect(Collectors.toList());
+    }
+
+    public void deleteProject(UUID projectId) {
+        Project project = projectRepository.findByUuid(projectId);
+
+        if (project != null) {
+            projectRepository.delete(project);
+        }
     }
 }
