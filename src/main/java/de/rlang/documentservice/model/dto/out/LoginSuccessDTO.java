fea2ec1cc49ba4c9df674c0af27971b64334fef2
Rico Lang <rico.lang@mackevision.com>
2019-03-11 17:52:50 +0100

    Decorate with AllArgsContructor

--- src/main/java/de/rlang/documentservice/model/dto/out/LoginSuccessDTO.java
+++ src/main/java/de/rlang/documentservice/model/dto/out/LoginSuccessDTO.java
@@ -2,9 +2,11 @@ package de.rlang.documentservice.model.dto.out;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class LoginSuccessDTO {
