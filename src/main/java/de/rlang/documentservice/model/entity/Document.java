f87f2b3c353ed349c21374d51f4771c37580d744
Rico Lang <Rico Lang>
2019-04-15 23:39:04 +0200

    Add document class

--- src/main/java/de/rlang/documentservice/model/entity/Document.java
+++ src/main/java/de/rlang/documentservice/model/entity/Document.java
@@ -1,4 +1,12 @@
 package de.rlang.documentservice.model.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "document")
+@Entity
 public class Document {
 }
