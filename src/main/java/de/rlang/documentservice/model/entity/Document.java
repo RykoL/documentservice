c0a1f4ec5b1c66b9853f56a68886d1e69784f008
Rico Lang <rico.lang15@gmail.com>
2019-04-16 11:11:57 +0200

    Implemented document entity

--- src/main/java/de/rlang/documentservice/model/entity/Document.java
+++ src/main/java/de/rlang/documentservice/model/entity/Document.java
@@ -1,12 +1,33 @@
 package de.rlang.documentservice.model.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Set;
+import java.util.UUID;
 
 @Data
 @Table(name = "document")
 @Entity
 public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID uuid;
+
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "document_id")
+    private Set<DocumentRevision> documentRevisions;
+
+    @PrePersist
+    public void fill() {
+        uuid = UUID.randomUUID();
+    }
 }
