07a8c9666c3d2476576c91780e44ae4125b4ebac
Rico Lang <rlang@mvision.lan>
2019-04-16 11:09:29 +0200

    Implement DocumentRevision entity

--- src/main/java/de/rlang/documentservice/model/entity/DocumentRevision.java
+++ src/main/java/de/rlang/documentservice/model/entity/DocumentRevision.java
@@ -1,4 +1,31 @@
 package de.rlang.documentservice.model.entity;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Table(name = "document_revision")
+@Entity
 public class DocumentRevision {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID uuid;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    private String storagePath;
+
+    private String comment;
+
 }
