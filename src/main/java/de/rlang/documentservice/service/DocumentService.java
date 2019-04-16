a9bddded78a0c22020753903c9cdf6e23fabeb61
Rico Lang <rico.lang15@gmail.com>
2019-04-16 14:50:38 +0200

    Implement storeFile function

--- src/main/java/de/rlang/documentservice/service/DocumentService.java
+++ src/main/java/de/rlang/documentservice/service/DocumentService.java
@@ -1,10 +1,20 @@
 package de.rlang.documentservice.service;
 
+import de.rlang.documentservice.exception.ForbiddenException;
+import de.rlang.documentservice.model.entity.Document;
+import de.rlang.documentservice.model.entity.DocumentRevision;
+import de.rlang.documentservice.model.entity.Project;
+import de.rlang.documentservice.model.entity.User;
 import de.rlang.documentservice.repository.DocumentRepository;
 import de.rlang.documentservice.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
 
 @Service
 public class DocumentService {
@@ -15,7 +25,32 @@ public class DocumentService {
     @Autowired
     private DocumentRepository documentRepository;
 
-    public void storeFile(MultipartFile multipartFile) {
+    @Autowired
+    private UserService userService;
 
+    @Autowired
+    private FilesystemService filesystemService;
+
+    @Transactional
+    public void storeFile(UUID projectId, MultipartFile multipartFile) throws IOException {
+        User currentUser = userService.getCurrentAuthenticatedUser();
+        Project project = projectRepository.findFirstByUuid(projectId);
+
+        if (!project.getParticipants().contains(currentUser)) {
+            throw new ForbiddenException();
+        }
+
+        Path p = filesystemService.saveToDisk(multipartFile);
+
+        DocumentRevision documentRevision = new DocumentRevision();
+        documentRevision.setMimeType(multipartFile.getContentType());
+        documentRevision.setStoragePath(p.toString());
+
+        Document document = new Document();
+        document.setName(multipartFile.getName());
+        document.getDocumentRevisions().add(documentRevision);
+
+        project.getDocuments().add(document);
+        projectRepository.save(project);
     }
 }
